3be10ca70036d50db1106cd11788d4bafbfb24e8
mikanbearer <50346402+mikanbearer@users.noreply.github.com>
2020-09-15 16:51:42 +0800

    fixed theme

--- docs/js/code-block.js
+++ docs/js/code-block.js
@@ -1,23 +1,20 @@
-
-
-
-/*
-
 var code_list = document.body.getElementsByTagName('code')
 
 for (i=0; i <= code_list.length-1; i++) {
   var codeParent = code_list[i].closest('pre')
-  if (!codeParent.closest('div.highlight')) {
-    if (/^language-:+/.test(code_list[i].className)) {
-      var div = document.createElement('div')
-      div.classList.add('highlight')
-      var pre = codeParent.cloneNode(true)
-      div.appendChild(pre)
-      codeParent.replaceWith(div)
+  if (codeParent) {
+    if (!codeParent.closest('div.highlight')) {
+      if (/^language-:+/.test(code_list[i].className)) {
+        var div = document.createElement('div')
+        div.classList.add('highlight')
+        var pre = codeParent.cloneNode(true)
+        div.appendChild(pre)
+        codeParent.replaceWith(div)
+      }
     }
   }
 }
-*/
+
 
 var list = document.body.getElementsByClassName('highlight')
 
